226904c04f9e1fe4df25c2992c176b4300233313
davegaly <davegaly@gmail.com>
2023-11-22 14:24:01 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './style.css';
+import { PrimeReactProvider } from 'primereact/api';
+import { Button } from 'primereact/button';  
+import { Dropdown } from 'primereact/dropdown';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export default function App() {
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic ha bnmnmbppen :)</p>
-    </div>
+    <PrimeReactProvider>
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic ha Aasdf :)</p>
+        <p><Button label="Submit" onClick={() => {alert('Hello, React!');}} /></p>
+      </div>
+    </PrimeReactProvider>
   );
 }
